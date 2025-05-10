f1d2f6fe9ec1a50491a386b4ccdf19084ddc4d3c
mwassiyazan@protonmail.com <109613511+Yazaven@users.noreply.github.com>
2025-05-10 22:12:02 +0300

    finalll

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-  }
+    cssCodeSplit: false 
 });
